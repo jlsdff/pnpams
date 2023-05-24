49c24834248c85324cd146645fafef1343d8ec25
Julius Duff <8215455@ntc.edu.ph>
2023-05-24 21:03:01 +0800

    navigation finished

--- src/app/attendance/page.tsx
+++ src/app/attendance/page.tsx
@@ -1,10 +1,14 @@
-import Link from 'next/link';
+/** @format */
+
+import Link from "next/link";
 
 export default function Attendance() {
     return (
         <>
-        <h1>Attendance</h1>
-        <Link href="/dashboard/records">dashboard</Link>
+            <h1>Attendance</h1>
+            <Link href="/dashboard/records">
+                dashboard
+            </Link>
         </>
-    )
+    );
 }
--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,9 +1,48 @@
 /** @format */
+import { Container, Navbar, Dropdown, Button } from "react-bootstrap";
+import styles from "./navigation.module.css";
 
 export default function Navigation({}) {
+
+    const admin = JSON.parse(sessionStorage.getItem("admin") || "{}");
+
+    function handleLogout() {}
+
     return (
-        <nav>
-            <h1>Navigation</h1>
-        </nav>
+        <Navbar>
+            <Container fluid>
+                <Navbar.Brand>
+                    <img
+                        src="/images/PNP-Logo.png"
+                        alt=""
+                        className={`${styles.logo}`}
+                    />
+                    <span>P.N.P. Attendance Monitoring System</span>
+                </Navbar.Brand>
+                <Dropdown align="end">
+                    <Dropdown.Toggle variant="light" id="dropdown-basic">
+                        <img
+                            src="/images/profile-icon.png"
+                            alt=""
+                            className={`${styles.profileIcon}`}
+                        />
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu className="p-3">
+                        {/* <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">
+                            Another action
+                        </Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">
+                            Something else
+                        </Dropdown.Item> */}
+                        <p>{`${admin.lastName}, ${admin.firstName}`}</p>
+                        <Dropdown.Divider />
+                        <Button onClick={handleLogout} variant="light">
+                            Logout
+                        </Button>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </Container>
+        </Navbar>
     );
 }
